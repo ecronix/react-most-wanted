0b78beed0f951dfcfcfa54ac0280d72ad48556c9
TarikHuber <huber.tarik@gmail.com>
2017-04-05 13:07:01 +0200

    Added travis file

--- src/tests/components/DrawerContent.test.js
+++ src/tests/components/DrawerContent.test.js
@@ -2,12 +2,48 @@ import React from 'react';
 import { DrawerContent } from '../../components/Drawer';
 import { shallowWithIntlTheme } from '../../helpers/intl-theme-test';
 
-const wrapper=shallowWithIntlTheme(<DrawerContent/>)
+
+function setup() {
+  const props = {
+    setResponsive: jest.fn(),
+    setDrawerOpen: jest.fn(),
+    updateLocale: jest.fn(),
+  }
+
+  const wrapper = shallowWithIntlTheme(<DrawerContent {...props} />)
+
+  return {
+    props,
+    wrapper
+  }
+}
 
 describe('components', () => {
   describe('DrawerContent', () => {
+    const { wrapper } = setup();
+
     it('renders without exploding', () => {
       expect(wrapper.length).toBe(1)
     })
+
+    it('should be able to call setResponsive', () => {
+      const { wrapper, props } = setup()
+      props.setResponsive(true)
+      expect(props.setResponsive.mock.calls.length).toBe(1)
+    })
+
+    it('should be able to call setDrawerOpen', () => {
+      const { wrapper, props } = setup()
+      props.setDrawerOpen(true)
+      expect(props.setDrawerOpen.mock.calls.length).toBe(1)
+    })
+
+    it('should be able to call updateLocale', () => {
+      const { wrapper, props } = setup()
+      props.updateLocale(true)
+      expect(props.updateLocale.mock.calls.length).toBe(1)
+    })
+
+
   })
 })
