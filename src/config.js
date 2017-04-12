b376212e235a9a0f988c68ed7e4cf1edc69fbf5e
TarikHuber <huber.tarik@gmail.com>
2017-04-12 14:10:59 +0200

    Config file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config= {
+  google_client_id: '941480856475-lvilroe1spibcocgo9rgo592hd67i17h.apps.googleusercontent.com',
+  facebook_app_id: '1832263543689561',
+}
+
+
+export default config;
