5b19a9f2abb449650e425c542f7d5fbcd0d8ab8a
TarikHuber <huber.tarik@gmail.com>
2017-05-15 11:56:25 +0200

    Bugfix for safari. Intl pollyfill!

--- src/locales/index.js
+++ src/locales/index.js
@@ -6,6 +6,26 @@ import en from 'react-intl/locale-data/en';
 import de from 'react-intl/locale-data/de';
 import bs from 'react-intl/locale-data/bs';
 
+var areIntlLocalesSupported = require('intl-locales-supported');
+
+var localesMyAppSupports = [
+    /* list locales here */
+];
+
+if (global.Intl) {
+    // Determine if the built-in `Intl` has the locale data we need.
+    if (!areIntlLocalesSupported(localesMyAppSupports)) {
+        // `Intl` exists, but it doesn't have the data we need, so load the
+        // polyfill and replace the constructors with need with the polyfill's.
+        var IntlPolyfill = require('intl');
+        Intl.NumberFormat   = IntlPolyfill.NumberFormat;
+        Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+    }
+} else {
+    // No `Intl`, so use and load the polyfill.
+    global.Intl = require('intl');
+}
+
 const locales = [
   {
     locale: 'en',
