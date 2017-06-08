aecb01567a1b16db9eb92379a93cfc74e311e604
tarikhuber <huber.tarik@gmail.com>
2017-06-08 15:39:42 +0200

    Integrated provider indicator into users list

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -11,6 +11,8 @@ import Divider from 'material-ui/Divider';
 import CircularProgress from 'material-ui/CircularProgress';
 import Avatar from 'material-ui/Avatar';
 import FontIcon from 'material-ui/FontIcon';
+import {GoogleIcon, FacebookIcon, GitHubIcon, TwitterIcon} from '../../components/Icons';
+import IconButton from 'material-ui/IconButton';
 
 const styles={
   center_container:{
@@ -32,8 +34,44 @@ class Users extends Component {
     this.props.unsubscribeList();
   }
 
+  getProviderIcon = (provider) => {
+
+    const {muiTheme} =this.props;
+
+    const color=muiTheme.palette.primary2Color;
+
+    switch (provider.providerId) {
+      case 'google.com':
+      return <IconButton key={provider.providerId}>
+        <GoogleIcon color={color}/>
+      </IconButton>
+      case 'facebook.com':
+      return <IconButton key={provider.providerId}>
+        <FacebookIcon color={color}/>
+      </IconButton>
+      case 'twitter.com':
+      return <IconButton key={provider.providerId}>
+        <TwitterIcon color={color}/>
+      </IconButton>
+      case 'github.com':
+      return <IconButton   key={provider.providerId}>
+        <GitHubIcon color={color}/>
+      </IconButton>
+      case 'phone':
+      return <IconButton   key={provider.providerId} >
+        <FontIcon className="material-icons" color={color} >phone</FontIcon>
+      </IconButton>
+      case 'password':
+      return <IconButton   key={provider.providerId}>
+        <FontIcon className="material-icons" color={color} >email</FontIcon>
+      </IconButton>
+      default:
+      return undefined;
+    }
+  }
+
   rednerList(users) {
-    const {intl} =this.props;
+    const {intl, muiTheme} =this.props;
 
 
     return _.map(users.list, (user, key) => {
@@ -43,15 +81,41 @@ class Users extends Component {
           key={key}
           id={key}
           leftAvatar={<Avatar src={user.photoURL} alt="person" icon={<FontIcon className="material-icons" >person</FontIcon>}/>}
-          rightIcon={<FontIcon className="material-icons" color={user.connections?'green':'red'}>offline_pin</FontIcon>}
-          primaryText={user.displayName}
-          secondaryTextLines={1}
-          secondaryText={<p>
-            {(!user.connections && !user.lastOnline)?intl.formatMessage({id: 'offline'}):intl.formatMessage({id: 'online'})}
-            {' '}
-            {(!user.connections && user.lastOnline)?intl.formatRelative(new Date(user.lastOnline)):undefined}
-          </p>}
-        />
+          rightIcon={<FontIcon className="material-icons" color={user.connections?'green':'red'}>offline_pin</FontIcon>}>
+
+          <div style={{display: 'flex'}}>
+            <div>
+              <div>
+                {user.displayName}
+              </div>
+              <div
+                style={{
+                  fontSize: 14,
+                  lineHeight: '16px',
+                  height: 16,
+                  margin: 0,
+                  marginTop: 4,
+                  color: muiTheme.listItem.secondaryTextColor,
+                }}>
+                <p>
+                  {(!user.connections && !user.lastOnline)?intl.formatMessage({id: 'offline'}):intl.formatMessage({id: 'online'})}
+                  {' '}
+                  {(!user.connections && user.lastOnline)?intl.formatRelative(new Date(user.lastOnline)):undefined}
+                </p>
+              </div>
+
+            </div>
+
+            <div style={{alignSelf: 'center', flexDirection: 'row', display: 'flex'}}>
+              {user.providerData && user.providerData.map(
+                (p)=>{
+                  return this.getProviderIcon(p);
+                })
+              }
+            </div>
+          </div>
+
+        </ListItem>
         <Divider inset={true}/>
       </div>
     });
