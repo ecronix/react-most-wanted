f765bfd1d5308bd2594640b0b66010ba31075e74
Tarik Huber <huber.tarik@gmail.com>
2018-03-20 10:20:52 +0100

    Added BrowserSTack to CI

--- before_install.sh
+++ before_install.sh
@@ -3,6 +3,7 @@
 #run only on master
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
    npm install -g firebase-tools
+   npm install -g selenium-webdriver
    npm install codecov.io coveralls
    cd functions
    npm install
--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
   firebase use prod --token $FIREBASE_TOKEN
   firebase deploy --non-interactive --token $FIREBASE_TOKEN
+  node bs.js $BSNAME $BSKEY
 fi
