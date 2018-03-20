b65e467ebfb1244f10a593ac5de9d6ff514dd4ba
Tarik Huber <huber.tarik@gmail.com>
2018-03-20 14:05:25 +0100

    Testing failed BrowserStack test

--- bs_test.js
+++ bs_test.js
@@ -1,12 +1,11 @@
 var webdriver = require('selenium-webdriver')
 
 module.exports.runTest = function (driver) {
-  driver.get('http://www.react-most-wanted.com').then(function () {
+  driver.get('http://www.react-most-wanted2.com').then(function () {
     var passwordButton = driver.wait(webdriver.until.elementLocated(webdriver.By.className('firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-password firebaseui-id-idp-button')))
     var emailInput = driver.wait(webdriver.until.elementLocated(webdriver.By.name('email')))
     var passwordInput = driver.wait(webdriver.until.elementLocated(webdriver.By.name('password')))
     var nextButton = driver.wait(webdriver.until.elementLocated(webdriver.By.className('firebaseui-id-submit firebaseui-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored')))
-    var signIn = driver.wait(webdriver.until.elementLocated(webdriver.By.className('firebaseui-id-submit firebaseui-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored')))
 
     passwordButton.click().then(() => {
       emailInput.sendKeys('test@test.com').then(() => {
@@ -19,5 +18,9 @@ module.exports.runTest = function (driver) {
         })
       })
     })
+  }).catch(e => {
+    driver.quit()
+    console.log('Test Failed')
+    process.exit(1)
   })
 }
