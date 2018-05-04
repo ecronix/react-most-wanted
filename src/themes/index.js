0a4af872cc9fabd33451bc29c80f75ba2bf8199c
Tarik Huber <huber.tarik@gmail.com>
2018-05-04 12:05:23 +0200

    Started migrating to MUI V1

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,15 +1,29 @@
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import { createMuiTheme } from 'material-ui/styles'
 import icsTheme from './ics_theme'
+import red from 'material-ui/colors/red'
+import pink from 'material-ui/colors/pink'
+import green from 'material-ui/colors/green'
+// import red from 'material-ui/colors/red'
 
 const themes = [
   {
     id: 'light',
-    source: lightBaseTheme
+    source: {
+      palette: {
+        primary: red,
+        secondary: green
+      }
+    }
   },
   {
     id: 'dark',
-    source: darkBaseTheme
+    source: {
+      palette: {
+        primary: red,
+        secondary: pink,
+        error: red
+      }
+    }
   },
   {
     id: 'ics',
