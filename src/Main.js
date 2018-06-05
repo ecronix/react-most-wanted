9eff7cc7eec657e1308c368a1bd0daf2d8c1a4c3
Tarik Huber <huber.tarik@gmail.com>
2018-06-05 14:57:17 +0200

    Updated rmw-shell

--- src/Main.js
+++ src/Main.js
@@ -5,23 +5,29 @@ import locales from './locales'
 import App from 'rmw-shell'
 import { Helmet } from 'react-helmet'
 import config from './config'
-import A2HSProvider from 'rmw-shell/lib/containers/A2HSProvider'
+import { withA2HS } from 'a2hs'
 
 addLocalizationData(locales)
 
 class Main extends Component {
-  render() {
+  componentDidMount () {
+    const { setA2HPState } = this.props
+    console.log(this.props)
+    // setA2HPState({ isAppInstallable: true })
+  }
+
+  render () {
     return <div>
       <Helmet>
         <link async type='text/css' rel='stylesheet' href='https://cdn.firebase.com/libs/firebaseui/3.0.0/firebaseui.css' />
         <link async rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
         <link async rel='stylesheet' href='index.css' />
       </Helmet>
-      <A2HSProvider>
-        <App appConfig={{ configureStore, ...config }} />
-      </A2HSProvider>
+
+      <App appConfig={{ configureStore, ...config }} />
+
     </div>
   }
 }
 
-export default Main
+export default withA2HS(Main)
