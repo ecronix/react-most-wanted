44502e006d98e4e1e86ce2e98ebb4e465ed4a4ce
Tarik Huber <huber.tarik@gmail.com>
2018-06-05 09:53:26 +0200

    Bugfix for theme color

--- src/Main.js
+++ src/Main.js
@@ -5,18 +5,21 @@ import locales from './locales'
 import App from 'rmw-shell'
 import { Helmet } from 'react-helmet'
 import config from './config'
+import A2HSProvider from 'rmw-shell/lib/containers/A2HSProvider'
 
 addLocalizationData(locales)
 
 class Main extends Component {
-  render () {
+  render() {
     return <div>
       <Helmet>
         <link async type='text/css' rel='stylesheet' href='https://cdn.firebase.com/libs/firebaseui/3.0.0/firebaseui.css' />
         <link async rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
         <link async rel='stylesheet' href='index.css' />
       </Helmet>
-      <App appConfig={{ configureStore, ...config }} />
+      <A2HSProvider>
+        <App appConfig={{ configureStore, ...config }} />
+      </A2HSProvider>
     </div>
   }
 }
