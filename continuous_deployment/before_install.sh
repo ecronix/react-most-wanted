4736887de575862910263e3eb6c5ac7c5b971ff5
TarikHuber <huber.tarik@gmail.com>
2018-08-09 22:16:40 +0200

    Updated dependencies

--- continuous_deployment/before_install.sh
+++ continuous_deployment/before_install.sh
@@ -5,7 +5,9 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; th
    npm install -g firebase-tools
    npm install -g selenium-webdriver
    npm install codecov.io coveralls
+   npm audit fix
    cd functions
    npm install
+   npm audit fix
    cd ..
 fi
