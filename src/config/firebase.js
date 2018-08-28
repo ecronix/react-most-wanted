05f45491debfd4009e584e3cca1e6c3f6edcb788
TarikHuber <huber.tarik@gmail.com>
2018-08-28 11:42:13 +0200

    Enabled offline capability for firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,27 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import config from './config'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+import 'firebase/firestore'
+import 'firebase/messaging'
 
-export const firebaseApp = firebase.initializeApp(process.env.NODE_ENV !== 'production' ? config.firebase_config_dev : config.firebase_config)
+const firebaseApp = firebase.initializeApp(process.env.NODE_ENV !== 'production' ? config.firebase_config_dev : config.firebase_config)
+
+firebase.firestore().enablePersistence()
+  .catch(function (err) {
+    if (err.code === 'failed-precondition') {
+      console.log('failed-precondition')
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === 'unimplemented') {
+      console.log('unimplemented')
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  })
+
+export { firebaseApp }
 export default firebaseApp
