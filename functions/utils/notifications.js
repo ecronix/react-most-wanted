9b3213d475dd964b0ce25eec7325d69f6e7cff93
Tarik Huber <tarik.huber@ics.local>
2019-10-31 14:14:49 +0100

    Updated dependencies and improved install prompt

--- functions/utils/notifications.js
+++ functions/utils/notifications.js
@@ -1,26 +1,67 @@
 const admin = require('firebase-admin')
-try { admin.initializeApp() } catch (e) { console.log(e) }
+const nodemailer = require('nodemailer')
 
 module.exports = {
-  notifyUser: (userUid, payload) => {
+  notifyUser: async (userUid, payload) => {
     console.log(userUid, payload)
 
-    return admin.database().ref(`/notification_tokens/${userUid}`).once('value').then(snapshot => {
-      let registrationTokens = []
+    const snapshot = await admin
+      .database()
+      .ref(`/notification_tokens/${userUid}`)
+      .once('value')
 
+    let registrationTokens = []
+
+    if (snapshot.exists()) {
       snapshot.forEach(token => {
         if (token.val()) {
           registrationTokens.push(token.key)
         }
       })
+    }
 
-      if (registrationTokens.length) {
-        return admin.messaging().sendToDevice(registrationTokens, payload)
-      } else {
-        console.log('Not tokens registered')
-        return null
+    if (registrationTokens.length) {
+      await admin.messaging().sendToDevice(registrationTokens, payload)
+    } else {
+      /*
+      if (functions.config().email_notifications) {
+        const emailNotificationSnap = admin()
+          .database()
+          .ref(`email_notifications/${userUid}`)
+          .once('value')
+
+      
+        if (emailNotificationSnap.exists()) {
+          const userSnap = await admin
+            .database()
+            .ref(`users/${userUid}`)
+            .once('value')
+
+          if (userSnap.exists()) {
+            const { email = false } = userSnap.val()
+            const { body = '', title = '', click_action = '' } = payload
+            const mailTransport = nodemailer.createTransport(functions.config().email_notifications.transport)
+
+            if (email) {
+              const mailOptions = {
+                to: email,
+                subject: title,
+                text: `${body}
+                ${click_action}`
+              }
+
+              await mailTransport.sendMail(mailOptions)
+              console.log('Notification email send!')
+            }
+          }
+        }
       }
-    })
-  }
+      */
 
+      console.log('Not tokens registered')
+      return null
+    }
+
+    return 1
+  }
 }
