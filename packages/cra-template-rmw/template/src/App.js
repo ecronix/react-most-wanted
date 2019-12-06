2885b1dc1c33a894a70104dae3b28104e4ade384
Tarik Huber <tarik.huber@ics.local>
2019-12-06 09:02:19 +0100

    First change to template

--- packages/cra-template-rmw/template/src/App.js
+++ packages/cra-template-rmw/template/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>RMW Template</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
