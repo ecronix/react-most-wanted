2d90b305f681ae103b7bf832d2eaf44c9ba70241
Tarik Huber <tarik.huber@ics.local>
2019-12-06 10:15:12 +0100

    Monorepo refactoring #57

--- continuous_deployment/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ev
-#run only on master
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-  npm run build
-fi
--- continuous_deployment/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-#run only on master
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-  firebase use prod --token $FIREBASE_TOKEN
-  firebase deploy --non-interactive --token $FIREBASE_TOKEN
-  node ./continuous_deployment/bs.js $BSNAME $BSKEY
-fi
