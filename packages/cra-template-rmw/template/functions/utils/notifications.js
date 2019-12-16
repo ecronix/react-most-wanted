ca08dedca619ef6a78f72299e00faca98ac9edc4
Tarik Huber <tarik.huber@ics.local>
2019-12-16 14:03:06 +0100

    Fixing folder bug

--- packages/cra-template-rmw/template/functions/utils/notifications.js
+++ packages/cra-template-rmw/template/functions/utils/notifications.js
@@ -1,26 +1,28 @@
 const admin = require('firebase-admin')
-try { admin.initializeApp() } catch (e) { console.log(e) }
 
 module.exports = {
   notifyUser: (userUid, payload) => {
     console.log(userUid, payload)
 
-    return admin.database().ref(`/notification_tokens/${userUid}`).once('value').then(snapshot => {
-      let registrationTokens = []
+    return admin
+      .database()
+      .ref(`/notification_tokens/${userUid}`)
+      .once('value')
+      .then(snapshot => {
+        let registrationTokens = []
 
-      snapshot.forEach(token => {
-        if (token.val()) {
-          registrationTokens.push(token.key)
+        snapshot.forEach(token => {
+          if (token.val()) {
+            registrationTokens.push(token.key)
+          }
+        })
+
+        if (registrationTokens.length) {
+          return admin.messaging().sendToDevice(registrationTokens, payload)
+        } else {
+          console.log('Not tokens registered')
+          return null
         }
       })
-
-      if (registrationTokens.length) {
-        return admin.messaging().sendToDevice(registrationTokens, payload)
-      } else {
-        console.log('Not tokens registered')
-        return null
-      }
-    })
-  }
-
+  },
 }
