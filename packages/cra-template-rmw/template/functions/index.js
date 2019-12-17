b7276099191dc03c2230118465902374642eb7b3
Tarik Huber <tarik.huber@ics.local>
2019-12-17 14:59:52 +0100

    Implemented es6 import to cloud functions loader

--- packages/cra-template-rmw/template/functions/index.js
+++ packages/cra-template-rmw/template/functions/index.js
@@ -1,5 +1,4 @@
-require = require('esm')(module /*, options*/)
-const load = require('./load/load')
+const loadFunctions = require('firebase-function-tools')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const settings = { timestampsInSnapshots: true }
@@ -8,4 +7,4 @@ const config = functions.config().firebase
 admin.initializeApp(config)
 admin.firestore().settings(settings)
 
-load.default(__dirname, exports)
+loadFunctions(__dirname, exports)
