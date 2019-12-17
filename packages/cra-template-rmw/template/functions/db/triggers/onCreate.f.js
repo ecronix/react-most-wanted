a6182be29aa69ae9327c526c5e7c064009d65616
Tarik Huber <tarik.huber@ics.local>
2019-12-17 14:59:52 +0100

    Implemented es6 import to cloud functions loader

--- packages/cra-template-rmw/template/functions/db/triggers/onCreate.f.js
+++ packages/cra-template-rmw/template/functions/db/triggers/onCreate.f.js
@@ -1,7 +1,5 @@
 import { database } from 'firebase-functions'
 
-export default database
-  .ref('/triggers/{roleUid}')
-  .onWrite((eventSnap, context) => {
-    return eventSnap.after.ref.set(null)
-  })
+export default database.ref('/triggers/{uid}').onWrite(snap => {
+  return snap.after.ref.set(null)
+})
