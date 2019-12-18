f2ffa061aac218da565dd4f62daae39ea4493890
Tarik Huber <tarik.huber@ics.local>
2019-12-18 09:01:39 +0100

    Fixed initialization of firebase in firebase functions

--- packages/cra-template-rmw/template/functions/index.js
+++ packages/cra-template-rmw/template/functions/index.js
@@ -1,10 +1,8 @@
 const loadFunctions = require('firebase-function-tools')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-const settings = { timestampsInSnapshots: true }
 const config = functions.config().firebase
 
 admin.initializeApp(config)
-admin.firestore().settings(settings)
 
 loadFunctions(__dirname, exports)
