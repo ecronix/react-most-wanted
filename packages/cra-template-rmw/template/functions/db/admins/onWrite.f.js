fddde6b290812bb1d47e8a76d7b1ede7c477fb72
Tarik Huber <tarik.huber@ics.local>
2020-01-31 22:56:11 +0100

    Implemented grans sync

---
+++ packages/cra-template-rmw/template/functions/db/admins/onWrite.f.js
@@ -0,0 +1,27 @@
+import * as functions from 'firebase-functions'
+import admin from 'firebase-admin'
+import { setClaim, removeClaim } from '../../utils/customClaims'
+
+export default functions
+  .region('europe-west1')
+  .database.ref('admins/{uid}/')
+  .onWrite(async (snap, context) => {
+    const value = snap.after.val()
+    const uid = context.params.uid
+
+    if (value) {
+      await admin
+        .firestore()
+        .doc(`/admins/${uid}/`)
+        .set({ isAdmin: value })
+      await customClaims.setClaim(uid, 'admin')
+    } else {
+      await admin
+        .firestore()
+        .doc(`/admins/${uid}/`)
+        .delete()
+      await customClaims.removeClaim(uid, 'admin')
+    }
+
+    return
+  })
