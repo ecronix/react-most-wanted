8a1158508357ced1957470469bb44ec2fadf85cf
Tarik Huber <tarik.huber@ics.local>
2020-02-12 14:05:30 +0100

    Refactored Toggle Button

--- packages/rmw-shell/src/components/ReduxFormFields/ToggleButton.js
+++ packages/rmw-shell/src/components/ReduxFormFields/ToggleButton.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const ToggleButton = props => {
-  const { input, checkedIcon, uncheckedIcon, ...rest } = props
+  const { input, checkedIcon, uncheckedIcon, tooltip = '', ...rest } = props
   const { value, onChange } = input
   const isToggled = value === true
 
   return (
-    <IconButton onClick={() => onChange(!isToggled)} {...rest}>
-      {isToggled && checkedIcon}
-      {!isToggled && uncheckedIcon}
-    </IconButton>
+    <Tooltip title={tooltip}>
+      <IconButton onClick={() => onChange(!isToggled)} {...rest}>
+        {isToggled && checkedIcon}
+        {!isToggled && uncheckedIcon}
+      </IconButton>
+    </Tooltip>
   )
 }
 
