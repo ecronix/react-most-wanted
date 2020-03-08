f53380bc4048ab4060f44faeb0392179b373e47b
Tarik Huber <tarik.huber@ics.local>
2020-03-08 23:58:37 +0100

    Integrated analytics to rmw-shell app

--- packages/rmw-shell/src/containers/App/App.js
+++ packages/rmw-shell/src/containers/App/App.js
@@ -7,16 +7,20 @@ import configureStore from '../../store'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 //import RootAsync from 'rmw-shell/lib/containers/Root'
+import Analytics from '../../containers/Analytics/Analytics'
 
 const Loading = () => <LoadingComponent />
 
 export const RootAsync = Loadable({
   loader: () => import('rmw-shell/lib/containers/Root'),
-  loading: Loading
+  loading: Loading,
 })
 
 const App = ({ appConfig }) => {
-  const store = appConfig && appConfig.configureStore ? appConfig.configureStore() : configureStore()
+  const store =
+    appConfig && appConfig.configureStore
+      ? appConfig.configureStore()
+      : configureStore()
   const configs = { ...config, ...appConfig }
   const { landingPage: LandingPage = false } = configs
 
@@ -25,7 +29,16 @@ const App = ({ appConfig }) => {
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
-            {LandingPage && <Route path="/" exact component={LandingPage} />}
+            {LandingPage && (
+              <Route path="/" exact>
+                <React.Fragment>
+                  <LandingPage />
+                  {configs.analyticsProps && (
+                    <Analytics {...configs.analyticsProps} />
+                  )}
+                </React.Fragment>
+              </Route>
+            )}
             <Switch>
               <Route>
                 <RootAsync appConfig={configs} />
