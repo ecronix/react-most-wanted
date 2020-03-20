c8f4960361d56fd6a5fd09f1041bd5e6275a88bd
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-03-20 22:20:05 +0600

    Changed company.js file to functional component -Important, handlePhotoUploadSuccess function wasn't defined and I kept it that way. Still undefined

--- packages/cra-template-rmw/template/src/components/Forms/Company.js
+++ packages/cra-template-rmw/template/src/components/Forms/Company.js
@@ -1,7 +1,7 @@
 import AvatarImageField from 'rmw-shell/lib/components/ReduxFormFields/AvatarImageField'
 import Business from '@material-ui/icons/Business'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import TextField from 'rmw-shell/lib/components/ReduxFormFields/TextField'
 import { Field, reduxForm, formValueSelector } from 'redux-form'
 import { ImageCropDialog } from 'rmw-shell/lib/containers/ImageCropDialog'
@@ -11,104 +11,103 @@ import { setDialogIsOpen } from 'rmw-shell/lib/store/dialogs/actions'
 import { withRouter } from 'react-router-dom'
 import { withTheme } from '@material-ui/core/styles'
 
-class Form extends Component {
-  render() {
-    const {
-      handleSubmit,
-      intl,
-      initialized,
-      setDialogIsOpen,
-      dialogs,
-      match,
-    } = this.props
+const Form = props => {
+  const {
+    handleSubmit,
+    intl,
+    initialized,
+    setDialogIsOpen,
+    dialogs,
+    match,
+    change,
+  } = props
 
-    const uid = match.params.uid
+  const uid = match.params.uid
 
-    return (
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          height: '100%',
-          width: '100%',
-          alignItems: 'stretch',
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
-        <button type="submit" style={{ display: 'none' }} />
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        height: '100%',
+        width: '100%',
+        alignItems: 'stretch',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      }}
+    >
+      <button type="submit" style={{ display: 'none' }} />
 
-        <div style={{ margin: 15, display: 'flex', flexDirection: 'column' }}>
-          <AvatarImageField
-            name="photoURL"
+      <div style={{ margin: 15, display: 'flex', flexDirection: 'column' }}>
+        <AvatarImageField
+          name="photoURL"
+          disabled={!initialized}
+          uid={uid}
+          change={change}
+          initialized={initialized}
+          icon={<Business fontSize="large" />}
+          intl={intl}
+          path={'companies'}
+        />
+
+        <div>
+          <Field
+            name="name"
             disabled={!initialized}
-            uid={uid}
-            change={this.props.change}
-            initialized={initialized}
-            icon={<Business fontSize="large" />}
-            intl={intl}
-            path={'companies'}
-          />
-
-          <div>
-            <Field
-              name="name"
-              disabled={!initialized}
-              component={TextField}
-              placeholder={intl.formatMessage({ id: 'name_hint' })}
-              label={intl.formatMessage({ id: 'name_label' })}
-            />
-          </div>
-
-          <div>
-            <Field
-              name="full_name"
-              disabled={!initialized}
-              component={TextField}
-              placeholder={intl.formatMessage({ id: 'full_name_hint' })}
-              label={intl.formatMessage({ id: 'full_name_label' })}
-            />
-          </div>
-
-          <div>
-            <Field
-              name="vat"
-              disabled={!initialized}
-              component={TextField}
-              placeholder={intl.formatMessage({ id: 'vat_hint' })}
-              label={intl.formatMessage({ id: 'vat_label' })}
-            />
-          </div>
-
-          <div>
-            <Field
-              name="description"
-              disabled={!initialized}
-              component={TextField}
-              multiline
-              rows={2}
-              placeholder={intl.formatMessage({ id: 'description_hint' })}
-              label={intl.formatMessage({ id: 'description_label' })}
-            />
-          </div>
-
-          <ImageCropDialog
-            path={`companies/${uid}`}
-            fileName={'photoURL'}
-            onUploadSuccess={s => {
-              this.handlePhotoUploadSuccess(s)
-            }}
-            open={dialogs.new_company_photo !== undefined}
-            src={dialogs.new_company_photo}
-            handleClose={() => {
-              setDialogIsOpen('new_company_photo', undefined)
-            }}
-            title={intl.formatMessage({ id: 'change_photo' })}
+            component={TextField}
+            placeholder={intl.formatMessage({ id: 'name_hint' })}
+            label={intl.formatMessage({ id: 'name_label' })}
           />
         </div>
-      </form>
-    )
-  }
+
+        <div>
+          <Field
+            name="full_name"
+            disabled={!initialized}
+            component={TextField}
+            placeholder={intl.formatMessage({ id: 'full_name_hint' })}
+            label={intl.formatMessage({ id: 'full_name_label' })}
+          />
+        </div>
+
+        <div>
+          <Field
+            name="vat"
+            disabled={!initialized}
+            component={TextField}
+            placeholder={intl.formatMessage({ id: 'vat_hint' })}
+            label={intl.formatMessage({ id: 'vat_label' })}
+          />
+        </div>
+
+        <div>
+          <Field
+            name="description"
+            disabled={!initialized}
+            component={TextField}
+            multiline
+            rows={2}
+            placeholder={intl.formatMessage({ id: 'description_hint' })}
+            label={intl.formatMessage({ id: 'description_label' })}
+          />
+        </div>
+
+        <ImageCropDialog
+          path={`companies/${uid}`}
+          fileName={'photoURL'}
+          onUploadSuccess={s => {
+            handlePhotoUploadSuccess(s) //handlePhotoUploadSuccess is not defined
+          }}
+          open={dialogs.new_company_photo !== undefined}
+          src={dialogs.new_company_photo}
+          handleClose={() => {
+            setDialogIsOpen('new_company_photo', undefined)
+          }}
+          title={intl.formatMessage({ id: 'change_photo' })}
+        />
+      </div>
+    </form>
+  )
 }
 
 Form.propTypes = {
