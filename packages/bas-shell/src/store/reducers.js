9e1132f1a91768ce320906cf66e59713daa50bf8
kachkov <kachkovintegra@gmail.com>
2020-04-07 14:29:20 +0300

    add locale store

--- packages/bas-shell/src/store/reducers.js
+++ packages/bas-shell/src/store/reducers.js
@@ -2,8 +2,9 @@ import { combineReducers } from 'redux'
 import initState from './init'
 import rootReducer from './rootReducer'
 import simpleValues from './simpleValues/reducer'
+import locale from './locale/reducer'
 
-export const appReducers = { simpleValues }
+export const appReducers = { simpleValues, locale, }
 
 const appReducer = combineReducers(appReducers)
 
