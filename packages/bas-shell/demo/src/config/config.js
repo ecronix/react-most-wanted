3e3dc5e33ffb7e888d8a833c3f28931011102a30
Tarik Huber <tarik.huber@ics.local>
2020-04-13 12:51:13 +0200

    Integrated react-intl to base-schell #106

--- packages/bas-shell/demo/src/config/config.js
+++ packages/bas-shell/demo/src/config/config.js
@@ -1,18 +1,23 @@
 import React, { lazy } from 'react'
+import locales from './locales'
 import routes from './routes'
 import { isAuthorised } from '../utils/auth'
 import getMenuItems from './menuItems'
 import LandingPage from '../pages/LandingPage/LandingPage'
+import parseLanguages from 'base-shell/lib/utils/localeTools'
 
 const Loading = () => <div>Loading...</div>
 
 const config = {
-  initial_state: {},
+  initial_state: {
+    locale: parseLanguages(['en', 'ru'], 'en'),
+  },
   auth: {
     isAuthenticated: isAuthorised,
     signInURL: '/signin',
   },
   routes,
+  locales,
   getMenuItems,
   pages: {
     LandingPage: LandingPage,
---
+++ packages/bas-shell/demo/src/config/locales/ru.js
@@ -0,0 +1,7 @@
+import { defineMessages } from 'react-intl'
+
+const messages = defineMessages({
+  about_page: 'Opis stranica',
+})
+
+export default messages
