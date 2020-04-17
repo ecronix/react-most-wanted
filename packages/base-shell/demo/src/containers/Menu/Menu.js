4ab7ce629c45bc2256454f65959dcca108206895
Tarik Huber <tarik.huber@ics.local>
2020-04-17 16:05:14 +0200

    Fixed intl menu in base-shell #106

--- packages/base-shell/demo/src/containers/Menu/Menu.js
+++ packages/base-shell/demo/src/containers/Menu/Menu.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react'
-import { logout } from '../../utils/auth'
-import { withRouter, NavLink } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
 import { default as withAppConfigs } from 'base-shell/lib/providers/ConfigProvider/withConfig'
 import { injectIntl } from 'react-intl'
-import { compose } from 'redux'
+import { logout } from '../../utils/auth'
 import { updateLocale } from 'base-shell/lib/store/locale/actions'
-const Menu = ({ history, appConfig, intl }) => {
+import { withRouter, NavLink } from 'react-router-dom'
 
-  const [selectedLocale, setLocale] = useState(intl.formatMessage({ id: appConfig.initial_state.locale }))
-
+const Menu = ({ history, appConfig, intl, updateLocale, locale = 'en' }) => {
   const handleSignOut = (user) => {
     logout()
-    history.push('/signin');
+    history.push('/signin')
   }
 
-  const itemsMenu = appConfig.getMenuItems({
-    intl,
-    auth: appConfig.auth,
-    locale: appConfig.initial_state.locale,
-    updateLocale,
-    handleSignOut
-  })
-    .filter(item => {
+  const itemsMenu = appConfig
+    .getMenuItems({
+      intl,
+      auth: appConfig.auth,
+      locale,
+      updateLocale,
+      handleSignOut,
+    })
+    .filter((item) => {
       return item.visible !== false
     })
 
   const getNestedItems = function (hostItem, hostIndex) {
-
     if (hostItem.nestedItems !== undefined) {
       let nestedItems = hostItem.nestedItems.filter(function (item) {
         return item.visible !== false
@@ -34,44 +33,42 @@ const Menu = ({ history, appConfig, intl }) => {
 
       return (
         <ul>
-          {
-            nestedItems.map((nestedItem, k) => {
-              return (
-                <React.Fragment key={k}>
-                  {
-                    hostItem.primaryTogglesNestedList ?
-                      (
-                        <>
-                          <input onChange={e => {
-                            if (nestedItem.onClick) {
-                              nestedItem.onClick()
-                            }
-                            setLocale(e.currentTarget.value);
-                            console.log(`change locale to ${nestedItem.primaryText}`, nestedItem.onClick);
-                          }} checked={selectedLocale === nestedItem.primaryText} type="radio" id={nestedItem.primaryText} name={hostItem.primaryText} value={nestedItem.primaryText}></input>
-                          <label htmlFor={nestedItem.primaryText}>{nestedItem.primaryText}</label><br />
-                        </>
-                      ) :
-                      (
-                        <>
-                          <li key={k}>
-                            {
-                              nestedItem.primaryText
-                            }
-                          </li>
-                          {
-                            getNestedItems(nestedItem, k)
-                          }
-                        </>
-                      )
-                  }
-                </React.Fragment>)
-            })
-          }
-        </ul>)
+          {nestedItems.map((nestedItem, k) => {
+            return (
+              <React.Fragment key={k}>
+                {hostItem.primaryTogglesNestedList ? (
+                  <>
+                    <input
+                      onChange={(e) => {
+                        if (nestedItem.onClick) {
+                          nestedItem.onClick()
+                        }
+                      }}
+                      checked={locale === nestedItem.key}
+                      type="radio"
+                      id={nestedItem.primaryText}
+                      name={hostItem.primaryText}
+                      value={nestedItem.primaryText}
+                    ></input>
+                    <label htmlFor={nestedItem.primaryText}>
+                      {nestedItem.primaryText}
+                    </label>
+                    <br />
+                  </>
+                ) : (
+                  <>
+                    <li key={k}>{nestedItem.primaryText}</li>
+                    {getNestedItems(nestedItem, k)}
+                  </>
+                )}
+              </React.Fragment>
+            )
+          })}
+        </ul>
+      )
     }
 
-    return null;
+    return null
   }
 
   return (
@@ -79,35 +76,46 @@ const Menu = ({ history, appConfig, intl }) => {
       <nav>
         <ul>
           Menu
-          {
-            itemsMenu.map((item, i) => {
-              return (
-                <React.Fragment key={i}>
-                  <li style={{ listStyleType: item.value ? 'dash' : 'none' }}>
-                    {
-                      item.value ? <NavLink style={{ textDecoration: 'none' }} activeStyle={{ background: 'grey', color: 'white' }} to={item.value}
-                        onClick={e => {
-                          if (item.onClick) {
-                            item.onClick()
-                          }
-                        }} >{item.primaryText}</NavLink>
-                        : item.primaryText
-                    }
-                  </li>
-                  {
-                    getNestedItems(item, i)
-                  }
-                </React.Fragment>
-              );
-            })
-          }
+          {itemsMenu.map((item, i) => {
+            return (
+              <React.Fragment key={i}>
+                <li style={{ listStyleType: item.value ? 'dash' : 'none' }}>
+                  {item.value ? (
+                    <NavLink
+                      style={{ textDecoration: 'none' }}
+                      activeStyle={{ background: 'grey', color: 'white' }}
+                      to={item.value}
+                      onClick={(e) => {
+                        if (item.onClick) {
+                          item.onClick()
+                        }
+                      }}
+                    >
+                      {item.primaryText}
+                    </NavLink>
+                  ) : (
+                    item.primaryText
+                  )}
+                </li>
+                {getNestedItems(item, i)}
+              </React.Fragment>
+            )
+          })}
         </ul>
       </nav>
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    locale: state.locale,
+  }
+}
+
 export default compose(
+  connect(mapStateToProps, { updateLocale }),
   injectIntl,
   withRouter,
-  withAppConfigs,
+  withAppConfigs
 )(Menu)
