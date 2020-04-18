27fd6a9189ca66db2a1eb175e56d27746ec3bb50
kachkov <kachkovintegra@gmail.com>
2020-04-18 21:30:17 +0300

    page wrapper for pages

--- packages/material-ui-shell/demo/src/pages/About/About.js
+++ packages/material-ui-shell/demo/src/pages/About/About.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { injectIntl } from 'react-intl'
+import Page from 'material-ui-shell/lib/containers/Page/Page'
 
 const AboutPage = ({ intl }) => {
-  return <div>{intl.formatMessage({ id: 'about' })}</div>
+  return <Page>{intl.formatMessage({ id: 'about' })}</Page>
 }
 
 export default injectIntl(AboutPage)
