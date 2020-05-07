85cd99f3c6fa841e753e9bd8bb1c13226a083176
Tarik Huber <tarik.huber@ics.local>
2020-05-08 00:00:33 +0200

    Implemented locale persistance for base-shell

--- packages/base-shell/src/containers/Layout/Layout.js
+++ packages/base-shell/src/containers/Layout/Layout.js
@@ -3,12 +3,11 @@ import getDefaultRoutes from '../../components/DefaultRoutes/DefaultRoutes'
 import LocaleContext from '../../providers/Locale/Context'
 import ConfigContext from '../../providers/Config/Context'
 import LocaleProvider from '../../providers/Locale/Provider'
-import { IntlProvider, useIntl } from 'react-intl'
+import { IntlProvider } from 'react-intl'
 import { Switch } from 'react-router-dom'
 import { getLocaleMessages } from '../../utils/locale'
 
 export const LayoutContent = () => {
-  const intl = useIntl()
   const { appConfig } = useContext(ConfigContext)
   const { components, routes: appRoutes = [], containers, locale: confLocale } =
     appConfig || {}
@@ -43,9 +42,9 @@ export const LayoutContent = () => {
 export const Layout = () => {
   const { appConfig } = useContext(ConfigContext)
   const { locale } = appConfig
-  const { defaultLocale } = locale || {}
+  const { defaultLocale, persistKey } = locale || {}
   return (
-    <LocaleProvider locale={defaultLocale}>
+    <LocaleProvider defaultLocale={defaultLocale} persistKey={persistKey}>
       <LayoutContent appConfig={appConfig} />
     </LocaleProvider>
   )
