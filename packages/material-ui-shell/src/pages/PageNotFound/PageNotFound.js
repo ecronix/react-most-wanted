7a89a740c9d4615907c03cf25a65fa672b02c0de
kachkov <kachkovintegra@gmail.com>
2020-05-07 13:13:56 +0300

    wrapped PageNotFound page in page container

--- packages/material-ui-shell/src/pages/PageNotFound/PageNotFound.js
+++ packages/material-ui-shell/src/pages/PageNotFound/PageNotFound.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { injectIntl } from 'react-intl'
-
+import Page from '../../containers/Page/Page'
 const PageNotFound = ({ intl }) => {
-  return <div>{intl.formatMessage({ id: 'page_not_found' })} MUI</div>
+  return <Page pageTitle={intl.formatMessage({ id: 'page_not_found' })} MUI>{intl.formatMessage({ id: 'page_not_found' })} MUI</Page>
 }
 export default injectIntl(PageNotFound)
