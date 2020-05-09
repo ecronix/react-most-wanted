22f5aea5ebde1958375a68179210ae7df009860b
kachkov <usemuse@mail.ru>
2020-05-09 15:53:06 +0300

    persist auth object

--- packages/material-ui-shell/cra-template-material-ui/template/src/utils/auth.js
+++ packages/material-ui-shell/cra-template-material-ui/template/src/utils/auth.js
@@ -1,38 +1,40 @@
-const localStorageAuthKey = 'bas-shell:isAuthorised'
-
+const localStorageAuthKey = 'material-ui-shell:auth'
 export function saveAuthorisation(user) {
-  if (typeof Storage !== 'undefined') {
-    try {
-      localStorage.setItem(localStorageAuthKey, Boolean(user))
-    } catch (ex) {
-      console.log(ex)
+    if (typeof Storage !== 'undefined') {
+        try {
+            user.isAuthorised = true;
+            localStorage.setItem(localStorageAuthKey, JSON.stringify(user))
+        } catch (ex) {
+            console.log(ex)
+        }
+    } else {
+        // No web storage Support.
     }
-  } else {
-    // No web storage Support.
-  }
 }
 
 export function logout() {
-  if (typeof Storage !== 'undefined') {
-    try {
-      localStorage.setItem(localStorageAuthKey, false)
-    } catch (ex) {
-      console.log(ex)
+    if (typeof Storage !== 'undefined') {
+        try {
+            const auth = JSON.parse(localStorage.getItem(localStorageAuthKey))
+            auth.isAuthorised = false;
+            localStorage.setItem(localStorageAuthKey, JSON.stringify(auth))
+        } catch (ex) {
+            console.log(ex)
+        }
+    } else {
+        // No web storage Support.
     }
-  } else {
-    // No web storage Support.
-  }
 }
 
 export function isAuthorised() {
-  try {
-    if (typeof Storage !== 'undefined') {
-      const auth = JSON.parse(localStorage.getItem(localStorageAuthKey))
-      return auth || false;
-    } else {
-      return false
+    try {
+        if (typeof Storage !== 'undefined') {
+            const auth = JSON.parse(localStorage.getItem(localStorageAuthKey))
+            return auth.isAuthorised || false;
+        } else {
+            return false
+        }
+    } catch (ex) {
+        return false
     }
-  } catch (ex) {
-    return false
-  }
 }
