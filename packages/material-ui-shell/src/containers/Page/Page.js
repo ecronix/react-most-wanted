f662047cdef5eac7cca6035a9f3a35e419368d89
kachkov <usemuse@mail.ru>
2020-05-09 12:08:38 +0300

    removing unused dependencies

--- packages/material-ui-shell/src/containers/Page/Page.js
+++ packages/material-ui-shell/src/containers/Page/Page.js
@@ -1,22 +1,11 @@
 import React, { useContext } from 'react'
 import clsx from 'clsx'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import Drawer from '@material-ui/core/Drawer'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import MailIcon from '@material-ui/icons/Mail'
 import { useIntl } from 'react-intl'
 import OnlineContext from 'base-shell/lib/providers/Online/Context'
 import MenuContext from '../../providers/Menu/Context'
@@ -109,7 +98,6 @@ const Page = ({ children, pageTitle }) => {
   }
 
   const classes = useStyles()
-  const theme = useTheme()
 
   const handleDrawerOpen = () => {
     if (isDesktop) {
