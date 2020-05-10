72f31e4815dc9c8b9d466f0aa614687f4bb31616
Tarik Huber <tarik.huber@ics.local>
2020-05-10 13:41:35 +0200

    Implemented isDesktop to the Menu Provider

--- packages/material-ui-shell/src/providers/Menu/Provider.js
+++ packages/material-ui-shell/src/providers/Menu/Provider.js
@@ -1,37 +1,38 @@
 import PropTypes from 'prop-types'
 import React, { useState, useEffect } from 'react'
 import Context from './Context'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 const Provider = ({ children, persistKey = 'menu' }) => {
-  const [isDesktopOpen, setDesktopOpen] = useState(false);
-  const [isMobileOpen, setMobileOpen] = useState(false);
-  const [isMini, setMini] = useState(false);
+  const [isDesktopOpen, setDesktopOpen] = useState(false)
+  const [isMobileOpen, setMobileOpen] = useState(false)
+  const [isMini, setMini] = useState(false)
+  const isDesktop = useMediaQuery('(min-width:600px)')
 
-function getPersist (){
-  let persistMenu = JSON.parse(localStorage.getItem(persistKey));
-  return persistMenu;
-}
-function savePersist (persistMenu){
-  localStorage.setItem(persistKey, JSON.stringify(persistMenu))
-}
+  function getPersist() {
+    let persistMenu = JSON.parse(localStorage.getItem(persistKey))
+    return persistMenu
+  }
+  function savePersist(persistMenu) {
+    localStorage.setItem(persistKey, JSON.stringify(persistMenu))
+  }
   useEffect(() => {
-    let persistMenu = getPersist();
+    let persistMenu = getPersist()
     if (persistMenu) {
       setDesktopOpen(persistMenu.isDesktopOpen === 'true')
       setMobileOpen(persistMenu.isMobileOpen === 'true')
       setMini(persistMenu.isMini === 'true')
-    }
-    else {
-      persistMenu ={... {isDesktopOpen,isMobileOpen,isMini}};
-      savePersist(persistMenu);
+    } else {
+      persistMenu = { ...{ isDesktopOpen, isMobileOpen, isMini } }
+      savePersist(persistMenu)
     }
   }, [persistKey])
 
   useEffect(() => {
     try {
-      let persistMenu = getPersist();
-      persistMenu.isDesktopOpen = isDesktopOpen;
-      savePersist(persistMenu);
+      let persistMenu = getPersist()
+      persistMenu.isDesktopOpen = isDesktopOpen
+      savePersist(persistMenu)
     } catch (error) {
       console.warn(error)
     }
@@ -39,20 +40,19 @@ function savePersist (persistMenu){
 
   useEffect(() => {
     try {
-      let persistMenu = getPersist();
-      persistMenu.isMini = isMini;
-      savePersist(persistMenu);
+      let persistMenu = getPersist()
+      persistMenu.isMini = isMini
+      savePersist(persistMenu)
     } catch (error) {
       console.warn(error)
     }
   }, [isMini, persistKey])
 
-
   useEffect(() => {
     try {
-      let persistMenu = getPersist();
-      persistMenu.isMobileOpen = isMobileOpen;
-      savePersist(persistMenu);
+      let persistMenu = getPersist()
+      persistMenu.isMobileOpen = isMobileOpen
+      savePersist(persistMenu)
     } catch (error) {
       console.warn(error)
     }
@@ -61,6 +61,7 @@ function savePersist (persistMenu){
   return (
     <Context.Provider
       value={{
+        isDesktop,
         isDesktopOpen,
         setDesktopOpen,
         isMobileOpen,
