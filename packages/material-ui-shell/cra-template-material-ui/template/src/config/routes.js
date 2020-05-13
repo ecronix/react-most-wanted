c5db76c89a487eec5b745facea4ef4c75bdb501f
Tarik Huber <tarik.huber@ics.local>
2020-05-13 14:55:16 +0200

    Fixed signin route issue #133

--- packages/material-ui-shell/cra-template-material-ui/template/src/config/routes.js
+++ packages/material-ui-shell/cra-template-material-ui/template/src/config/routes.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-key */
 import React, { lazy } from 'react'
 import PrivateRoute from 'base-shell/lib/components/PrivateRoute/PrivateRoute'
+import PublicRoute from 'base-shell/lib/components/PublicRoute/PublicRoute'
 import { Route } from 'react-router-dom'
 
 const SignIn = lazy(() => import('../pages/SignIn/SignIn'))
@@ -8,7 +9,7 @@ const About = lazy(() => import('../pages/About/About'))
 const Home = lazy(() => import('../pages/Home/Home'))
 
 const routes = [
-  <Route path="/signin" exact component={SignIn} />,
+  <PublicRoute path="/signin" redirectTo="/" exact component={SignIn} />,
   <Route path="/about" exact component={About} />,
   <PrivateRoute path="/home" exact component={Home} />,
 ]
