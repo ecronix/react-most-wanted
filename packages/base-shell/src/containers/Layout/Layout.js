ca197950d3f30e384cd836a0a8f813d2d4afd34c
kachkov <usemuse@mail.ru>
2020-05-23 01:19:23 +0300

    not used by msg from eslint

--- packages/base-shell/src/containers/Layout/Layout.js
+++ packages/base-shell/src/containers/Layout/Layout.js
@@ -7,30 +7,30 @@ import { IntlProvider } from 'react-intl'
 import { Switch } from 'react-router-dom'
 import { getLocaleMessages } from '../../utils/locale'
 import '@formatjs/intl-relativetimeformat/polyfill'
-import areIntlLocalesSupported from 'intl-locales-supported'
-import intl from 'intl'
+// import areIntlLocalesSupported from 'intl-locales-supported'
+// import intl from 'intl'
 
-const loadLocalePolyfill = (locale) => {
-  // START: Intl polyfill
-  // Required for working on Safari
-  // Code from here: https://formatjs.io/guides/runtime-environments/
-  let localesMyAppSupports = [locale]
+// const loadLocalePolyfill = (locale) => {
+//   // START: Intl polyfill
+//   // Required for working on Safari
+//   // Code from here: https://formatjs.io/guides/runtime-environments/
+//   let localesMyAppSupports = [locale]
 
-  if (global.Intl) {
-    // Determine if the built-in `Intl` has the locale data we need.
-    if (!areIntlLocalesSupported(localesMyAppSupports)) {
-      // `Intl` exists, but it doesn't have the data we need, so load the
-      // polyfill and replace the constructors with need with the polyfill's.
-      let IntlPolyfill = intl
-      Intl.NumberFormat = IntlPolyfill.NumberFormat
-      Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
-    }
-  } else {
-    // No `Intl`, so use and load the polyfill.
-    global.Intl = intl
-  }
-  // END: Intl polyfill
-}
+//   if (global.Intl) {
+//     // Determine if the built-in `Intl` has the locale data we need.
+//     if (!areIntlLocalesSupported(localesMyAppSupports)) {
+//       // `Intl` exists, but it doesn't have the data we need, so load the
+//       // polyfill and replace the constructors with need with the polyfill's.
+//       let IntlPolyfill = intl
+//       Intl.NumberFormat = IntlPolyfill.NumberFormat
+//       Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
+//     }
+//   } else {
+//     // No `Intl`, so use and load the polyfill.
+//     global.Intl = intl
+//   }
+//   // END: Intl polyfill
+// }
 
 export const LayoutContent = () => {
   const [messages, setMessages] = useState([])
@@ -43,20 +43,20 @@ export const LayoutContent = () => {
   const defaultRoutes = getDefaultRoutes(appConfig)
   const { locale } = useContext(LocaleContext)
 
-  useEffect(() => {
-    const loadPolyfills = async () => {
-      loadLocalePolyfill(locale)
+  // useEffect(() => {
+  //   const loadPolyfills = async () => {
+  //     loadLocalePolyfill(locale)
 
-      for (let i = 0; i < locales.length; i++) {
-        const l = locales[i]
-        if (l.locale === locale) {
-          if (l.loadData) {
-            await l.loadData
-          }
-        }
-      }
-    }
-  }, [locale,locales])
+  //     for (let i = 0; i < locales.length; i++) {
+  //       const l = locales[i]
+  //       if (l.locale === locale) {
+  //         if (l.loadData) {
+  //           await l.loadData
+  //         }
+  //       }
+  //     }
+  //   }
+  // }, [locale,locales])
 
   useEffect(() => {
     const loadMessages = async () => {
@@ -64,7 +64,7 @@ export const LayoutContent = () => {
       setMessages(messages)
     }
     loadMessages()
-  }, [locale,locales])
+  }, [locale, locales])
 
   return (
     <IntlProvider locale={locale} key={locale} messages={messages}>
