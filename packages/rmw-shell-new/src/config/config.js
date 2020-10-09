3d221cdd60268b76cdb61e1a3c0ea93facc6d777
Tarik Huber <huber.tarik@gmail.com>
2020-10-09 23:26:39 +0200

    Fixed bug with blank page on reload

--- packages/rmw-shell-new/src/config/config.js
+++ packages/rmw-shell-new/src/config/config.js
@@ -1,25 +1,25 @@
-import MUIConfig from 'material-ui-shell'
-import getDefaultRoutes from './routes'
-import grants from './grants'
-import merge from 'base-shell/lib/utils/config'
-import { lazy } from 'react'
+import MUIConfig from "material-ui-shell";
+import getDefaultRoutes from "./routes";
+import grants from "./grants";
+import merge from "base-shell/lib/utils/config";
+import { lazy } from "react";
 
 const config = {
   getDefaultRoutes,
   containers: {
     LayoutContainer: lazy(() =>
-      import('../containers/LayoutContainer/LayoutContainer')
+      import("../containers/LayoutContainer/LayoutContainer")
     ),
   },
   components: {
-    Menu: lazy(() => import('../containers/FirebaseMenu/FirebaseMenu')),
+    Menu: lazy(() => import("../containers/FirebaseMenu/FirebaseMenu")),
   },
   auth: {
-    persistKey: 'rmw-shell:auth',
-    signInURL: '/signin',
-    redirectTo: '/home',
+    persistKey: "rmw-shell:auth",
+    signInURL: "/signin",
+    redirectTo: "/home",
     grants,
   },
-}
+};
 
-export default merge(MUIConfig, config)
+export default merge(MUIConfig, config);
