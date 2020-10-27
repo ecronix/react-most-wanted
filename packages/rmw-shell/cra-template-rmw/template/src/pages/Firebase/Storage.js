b63cbfcdf8708c14322c18ebfd4d8866f2fcd63f
Tarik Huber <huber.tarik@gmail.com>
2020-10-28 00:16:47 +0100

    Fixed storage provider bug

--- packages/rmw-shell/cra-template-rmw/template/src/pages/Firebase/Storage.js
+++ packages/rmw-shell/cra-template-rmw/template/src/pages/Firebase/Storage.js
@@ -19,7 +19,7 @@ export default function () {
     getUploadError,
     isUploading,
     getDownloadURL,
-    hasUploadError,
+    hasUploadError = () => {},
     uploadFile,
     clearUpload,
     getUploadProgress,
