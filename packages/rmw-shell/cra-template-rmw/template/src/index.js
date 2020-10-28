0afc15f4edb318eda0b61246089f94d5fc7f5411
Tarik Huber <huber.tarik@gmail.com>
2020-10-28 14:41:23 +0100

    Updated demplate

--- packages/rmw-shell/cra-template-rmw/template/src/index.js
+++ packages/rmw-shell/cra-template-rmw/template/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 render(
   <React.StrictMode>
