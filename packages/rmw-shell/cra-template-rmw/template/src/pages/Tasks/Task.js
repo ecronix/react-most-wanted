4ba69197d1ba967335bea28276e047ef2f217e7d
Tarik Huber <huber.tarik@gmail.com>
2020-10-28 14:22:53 +0100

    Updated template

--- packages/rmw-shell/cra-template-rmw/template/src/pages/Tasks/Task.js
+++ packages/rmw-shell/cra-template-rmw/template/src/pages/Tasks/Task.js
@@ -38,7 +38,6 @@ export default function () {
         }
       }}
       handleSubmit={(values, newUid) => {
-        console.log('values', values)
         if (newUid) {
           history.replace(`/${path}/${newUid}`)
         } else {
