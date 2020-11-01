4d76ce0157a63f60fcf5480055c5d4d7ceb1fa46
Tarik Huber <huber.tarik@gmail.com>
2020-11-01 23:22:26 +0100

    Updated CD

--- packages/rmw-shell/cra-template-rmw/template/continuous_deployment/before_install.sh
+++ packages/rmw-shell/cra-template-rmw/template/continuous_deployment/before_install.sh
@@ -2,6 +2,7 @@
 set -ev
 #run only on master
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
+   ls -l
    npm install -g firebase-tools
    npm install -g selenium-webdriver
    npm install
