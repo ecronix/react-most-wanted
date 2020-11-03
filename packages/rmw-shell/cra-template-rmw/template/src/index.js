26baa28e7a4d744b1d95feb50fe7d745596de398
Tarik Huber <huber.tarik@gmail.com>
2020-11-03 23:15:23 +0100

    Updated template

--- packages/rmw-shell/cra-template-rmw/template/src/index.js
+++ packages/rmw-shell/cra-template-rmw/template/src/index.js
@@ -13,4 +13,9 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register({
+  onUpdate: (reg) => {
+    console.log('Update', reg)
+    window.update = reg.waiting.postMessage({ type: 'SKIP_WAITING' })
+  },
+})
