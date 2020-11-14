deafd3f7bfbb6c84bd0fef03d146a523eaa28991
Tarik Huber <huber.tarik@gmail.com>
2020-11-14 13:29:38 +0100

    Updated template

--- packages/rmw-shell/cra-template-rmw/template/src/pages/Demo/Admin/index.js
+++ packages/rmw-shell/cra-template-rmw/template/src/pages/Demo/Admin/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Page from 'material-ui-shell/lib/containers/Page'
 import { useIntl } from 'react-intl'
 import { useHistory } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import { useFirebase } from 'rmw-shell/lib/providers/Firebase'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const Admin = () => {
   const intl = useIntl()
+  const [isLoading, setLoading] = useState(false)
   const history = useHistory()
   const { firebaseApp } = useFirebase()
 
@@ -26,27 +28,33 @@ const Admin = () => {
       >
         <p></p>REMOVE THIS IN PRODUCTION!!!
         <p>And delete the cloud function "httpsAdminOnCall"</p>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={async () => {
-            const httpsAdminOnCall = firebaseApp
-              .functions()
-              .httpsCallable('httpsAdminOnCall')
+        {isLoading && <CircularProgress />}
+        {!isLoading && (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={async () => {
+              setLoading(true)
+              const httpsAdminOnCall = firebaseApp
+                .functions()
+                .httpsCallable('httpsAdminOnCall')
 
-            const { data } = await httpsAdminOnCall()
-            const { message } = data || {}
+              const { data } = await httpsAdminOnCall()
+              const { message } = data || {}
 
-            if (message === 'OK') {
-              history.push('/users')
-            }
-          }}
-        >
-          {intl.formatMessage({
-            id: 'make_me_admin',
-            defaultMessage: 'Make me admin',
-          })}
-        </Button>
+              setLoading(false)
+
+              if (message === 'OK') {
+                history.push('/users')
+              }
+            }}
+          >
+            {intl.formatMessage({
+              id: 'make_me_admin',
+              defaultMessage: 'Make me admin',
+            })}
+          </Button>
+        )}
       </div>
     </Page>
   )
