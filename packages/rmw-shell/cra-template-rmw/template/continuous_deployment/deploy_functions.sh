f1b2e3b92d3e1f4257cd1b61271f70e6c6350c41
Tarik Huber <huber.tarik@gmail.com>
2020-12-24 00:19:34 +0100

    Updated travis

--- packages/rmw-shell/cra-template-rmw/template/continuous_deployment/deploy_functions.sh
+++ packages/rmw-shell/cra-template-rmw/template/continuous_deployment/deploy_functions.sh
@@ -3,9 +3,7 @@ set -ev
 #run only on master
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
   firebase use prod --token $FIREBASE_TOKEN
-  firebase deploy --except functions --non-interactive --token $FIREBASE_TOKEN
   cd functions
   firebase deploy --only functions --non-interactive --token $FIREBASE_TOKEN
   cd ..
-  node ./continuous_deployment/bs.js $BSNAME $BSKEY
 fi
