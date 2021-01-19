07d4c0e213b679e830caa83ff29201ba97d6efb6
jswelsh <james.sinclair.welsh@gmail.com>
2021-01-18 19:37:35 -0800

    fixed i think

--- packages/material-ui-shell/src/components/MenuHeader/MenuHeader.js
+++ packages/material-ui-shell/src/components/MenuHeader/MenuHeader.js
@@ -9,7 +9,6 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import PersonIcon from '@material-ui/icons/Person'
 import { useAuth } from 'base-shell/lib/providers/Auth'
@@ -21,10 +20,6 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp'
 import Brightness4Icon from '@material-ui/icons/Brightness4'
 import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh'
-import { Typography } from '@material-ui/core'
-
-
-import { Switch, useLocation } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -34,21 +29,6 @@ const useStyles = makeStyles((theme) => ({
   },
   listItem: {
     color: theme.palette.primary.contrastText,
-    cursor: 'pointer',
-  },
-  breadie: {
-    marginLeft:'2px',
-    display:'inline',
-    fontSize: '12px',
-    color: theme.palette.primary.contrastText,
-    cursor: 'pointer',
-
-  },
-  bread: {
-    marginLeft:'10px',
-    marginRight:'5px',
-    display:'inline',
-    color: '#bdbdbd',
     cursor: 'pointer',
   },
   icon: {
@@ -66,7 +46,6 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const MenuHeader = () => {
-  const location = useLocation()
   const theme = useTheme()
   const { auth } = useAuth()
   const { type, setType } = useAppTheme()
@@ -193,17 +172,6 @@ const MenuHeader = () => {
             )}
           </ListItem>
         )}
-        {!isMiniMode && (
-        <>
-          <Divider />
-          <ListItemText 
-            >
-              <Typography className={classes.bread} display="inline">
-                {location.pathname}
-              </Typography>
-            </ListItemText>
-        </>
-      )}
       </List>
     </Paper>
   )
