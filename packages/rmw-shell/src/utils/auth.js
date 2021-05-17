0f8de0e73db4eb67493fc722b9531471d15ebfd7
Tarik Huber <huber.tarik@gmail.com>
2021-05-17 16:44:23 +0200

    Added isAnyGranted to auth

--- packages/rmw-shell/src/utils/auth.js
+++ packages/rmw-shell/src/utils/auth.js
@@ -31,4 +31,31 @@ const isGranted = (auth, grant) => {
   return !!grants[grant]
 }
 
-export { defaultUserData, isGranted }
+const isAnyGranted = (auth, grantsToCheck = []) => {
+  const { grants = [], isAdmin = false } = auth || {}
+
+  if (isAdmin) {
+    return true
+  }
+
+  if (!grants) {
+    return false
+  }
+  if (!grantsToCheck) {
+    return false
+  }
+
+  let granted = false
+
+  for (let i = 0; i < grantsToCheck.length; i++) {
+    const grant = grantsToCheck[i]
+
+    if (isGranted(auth, grant)) {
+      granted = true
+    }
+  }
+
+  return granted
+}
+
+export { defaultUserData, isGranted, isAnyGranted }
