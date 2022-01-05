e9a808fadd4ab857ee0454e687f6cb4ce253858a
Robin <robin.braumann@gmail.com>
2022-01-05 08:22:53 +0100

    Add Bootstrap ThemeProvider

--- packages/bootstrap-shell/src/containers/AppContainer/AppContainer.js
+++ packages/bootstrap-shell/src/containers/AppContainer/AppContainer.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useConfig } from 'base-shell/lib/providers/Config'
+import { ThemeProvider } from "react-bootstrap"
 
 export default function ({ children }) {
   const { appConfig } = useConfig()
 
   return (
     <React.Fragment>
-      <div
-        style={{
-          display: 'flex',
-        }}
-      >
-        {children}
-      </div>
+      <ThemeProvider>
+        <div
+          style={{
+            display: 'flex',
+          }}
+        >
+          {children}
+        </div>
+      </ThemeProvider>
     </React.Fragment>
   )
 }
