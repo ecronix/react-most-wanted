4ec32713fe88735a04bdae51ca328d13b13c7674
Tarik Huber <huber.tarik@gmail.com>
2022-01-11 11:10:39 +0100

    test 5

--- packages/rmw-shell/cra-template-rmw/template/src/pages/LandingPage/index.js
+++ packages/rmw-shell/cra-template-rmw/template/src/pages/LandingPage/index.js
@@ -7,7 +7,7 @@ import CircularProgress from '@mui/material/CircularProgress'
 import { Helmet } from 'react-helmet'
 import { Scrollbars } from 'react-custom-scrollbars-2'
 import { useNavigate } from 'react-router-dom'
- 
+
 const PageContent = lazy(() => import('./PageContent'))
 const Footer = lazy(() => import('./Footer'))
 const ResponsiveMenu = lazy(() =>
@@ -258,6 +258,5 @@ const LandingPage = () => {
     </React.Fragment>
   )
 }
-
 
 export default LandingPage
