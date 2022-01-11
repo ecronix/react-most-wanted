d8574e27992d45d5d0508ead659400ebc4e6026d
Robin <robin.braumann@gmail.com>
2022-01-11 22:42:39 +0100

    update base-shell version

--- packages/bootstrap-shell/src/containers/LayoutContainer/LayoutContainer.js
+++ packages/bootstrap-shell/src/containers/LayoutContainer/LayoutContainer.js
@@ -5,12 +5,18 @@ import * as BS from "react-bootstrap";
 
 export default function ({ children }) {
   const { appConfig } = useConfig()
-  console.log("eeee")
+
   return (
     <BS.ThemeProvider >
       <MenuProvider>
         {children}
       </MenuProvider>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossOrigin="anonymous"
+      />
     </BS.ThemeProvider>
   )
 }
