631e1c21b23949af3f8cc36bd8f3ec5c7d1a7bfa
Robin <robin.braumann@gmail.com>
2022-01-12 21:17:26 +0100

    Create Menu Provider

---
+++ packages/bootstrap-shell/cra-template-bs/template/src/config/themes.js
@@ -0,0 +1,35 @@
+import red from '@mui/material/colors/red'
+
+const themes = [
+    {
+        id: 'default',
+    },
+    {
+        id: 'dark',
+        white: 'text-white',
+        source: {
+            bg: 'dark',
+            bgColor: 'bg-dark',
+            palette: {
+                primary: white,
+                secondary: white,
+                error: red,
+            },
+        },
+    },
+    {
+        id: 'custom',
+        white: 'text-white',
+        source: {
+            bg: 'dark',
+            bgColor: 'bg-dark',
+            palette: {
+                primary: white,
+                secondary: white,
+                error: red,
+            },
+        },
+    },
+]
+
+export default themes
