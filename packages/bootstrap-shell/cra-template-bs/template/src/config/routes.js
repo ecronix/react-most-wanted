ecf59f6f74005c3b5fa336a75912f0397413537b
Tarik Huber <huber.tarik@gmail.com>
2022-01-12 08:55:24 +0100

    Updated bs shell

--- packages/bootstrap-shell/cra-template-bs/template/src/config/routes.js
+++ packages/bootstrap-shell/cra-template-bs/template/src/config/routes.js
@@ -5,27 +5,27 @@ import React, { lazy } from 'react'
 
 const Home = lazy(() => import('../pages/Home/Home'))
 const routes = [
-    {
-        generateHeaderLink: true,
-        displayName: "Home",
-        path: '#home',
-        exact: true,
-        element: <Home redirectTo="#home" />
-    },
-    {
-        generateHeaderLink: true,
-        displayName: "Second",
-        path: '#home',
-        exact: true,
-        element: <Home redirectTo="#home" />
-    },
-    {
-        generateHeaderLink: true,
-        displayName: "Third",
-        path: '#home',
-        exact: true,
-        element: <Home redirectTo="#home" />
-    },
+  {
+    generateHeaderLink: true,
+    displayName: 'Home',
+    path: '/', // root path
+    exact: true,
+    element: <Home redirectTo="#home" />,
+  },
+  {
+    generateHeaderLink: true,
+    displayName: 'Second',
+    path: '/home2', // path /home2
+    exact: true,
+    element: <Home redirectTo="#home" />,
+  },
+  {
+    generateHeaderLink: true,
+    displayName: 'Third',
+    path: '*', //Page not found
+    exact: true,
+    element: <Home redirectTo="#home" />,
+  },
 ]
 
 export default routes
