ac73835512336a438cbd8289cbe142e731a71372
Robin <robin.braumann@gmail.com>
2022-01-16 23:48:14 +0100

    Implement nested dropdown for mobile

--- packages/bootstrap-shell/cra-template-bs/template/src/components/Menu/MenuRight.js
+++ packages/bootstrap-shell/cra-template-bs/template/src/components/Menu/MenuRight.js
@@ -5,6 +5,7 @@ import { BsFacebook, BsInstagram, BsGithub } from "react-icons/bs";
 
 export default function MenuRight() {
     return (<>
+        <BS.Nav.Link className="text-light" href='/home'>Hallo</BS.Nav.Link>
         <BS.Nav.Link className="text-light" href='/home'><BsFacebook /></BS.Nav.Link>
         <BS.Nav.Link className="text-light" href='/home'><BsInstagram /></BS.Nav.Link>
         <BS.Nav.Link className="text-light" href='https://github.com/TarikHuber/react-most-wanted'><BsGithub /></BS.Nav.Link>
--- packages/bootstrap-shell/src/components/Menu/MenuDropdownMobile.js
+++ packages/bootstrap-shell/src/components/Menu/MenuDropdownMobile.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as BS from "react-bootstrap"
+import { BsChevronDown } from "react-icons/bs"
 
 export default function MenuDropdownMobile({ navItem, className }) {
-
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        navItem &&
-        <BS.NavDropdown className={className} menuVariant='dark' title={navItem.displayName}>
-            {navItem?.nested?.map(item => {
-                console.log(item);
-                if (item.nested) {
-                    return <MenuDropdownMobile className="dropdown-submenu-mobile" navItem={item} />;
-                }
-                else {
-                    return <BS.NavDropdown.Item className="text-light" href={navItem.path}>{item.displayName}</BS.NavDropdown.Item>
-                }
+        <BS.Col
+            className="top-0 float-right bg-dark p-2 overflow-hidden"
+            style={{
+                maxHeight: menuOpen ? 500 : 30,
+                transition: 'max-height 2s linear',
+            }}
+        >
+            <div style={{
+                color: 'white', padding: '0 0.5rem',
+            }} onClick={() => setMenuOpen(!menuOpen)}>{navItem.displayName} <BsChevronDown /></div>
+            {navItem.nested?.map(menuItem => {
+                return menuItem.nested ? <MenuDropdownMobile style={{ position: 'relative' }} key={menuItem.displayName} navItem={menuItem} /> :
+                    <BS.Nav.Link key={menuItem.displayName} href={menuItem.path} className="text-light">
+                        {menuItem.displayName}
+                    </BS.Nav.Link>
             })}
-        </BS.NavDropdown>
-
+        </BS.Col>
     )
 }
