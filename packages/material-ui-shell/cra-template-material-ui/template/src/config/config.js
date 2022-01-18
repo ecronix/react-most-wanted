c16341176c025a596b6a07dbb63fd7b8cd4a12bc
Tarik Huber <huber.tarik@gmail.com>
2022-01-18 09:45:29 +0100

    actions

--- packages/material-ui-shell/cra-template-material-ui/template/src/config/config.js
+++ packages/material-ui-shell/cra-template-material-ui/template/src/config/config.js
@@ -27,7 +27,7 @@ const config = {
     locales,
     defaultLocale: parseLanguages(['en', 'de', 'ru'], 'en'),
     onError: (e) => {
-      // Here we can warn the user about translation error
+      // Here we warn the user about translation error
       //console.warn(e)
       return
     },
