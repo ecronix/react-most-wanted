71367c8a32cf6834e3832f0491132df786069be2
Tarik Huber <huber.tarik@gmail.com>
2022-08-07 20:22:08 +0200

    rmw ci test 2

--- packages/rmw-shell/cra-template-rmw/template/src/index.js
+++ packages/rmw-shell/cra-template-rmw/template/src/index.js
@@ -10,7 +10,7 @@ render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work  offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register({
