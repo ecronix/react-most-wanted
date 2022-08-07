8cab66364818425933e434fe3a9a478c1bc14197
Tarik Huber <huber.tarik@gmail.com>
2022-08-07 19:10:01 +0200

    Updated dependencies

--- packages/rmw-shell/cra-template-rmw/template/src/pages/Demo/Tasks/Task.js
+++ packages/rmw-shell/cra-template-rmw/template/src/pages/Demo/Tasks/Task.js
@@ -1,52 +1,52 @@
-import Form from '../../../components/Forms/Task'
-import React, { useEffect } from 'react'
-import { FormPage } from 'rmw-shell/lib/containers/Page'
-import { useIntl } from 'react-intl'
-import { useParams, useNavigate } from 'react-router-dom'
-import { useLists } from 'rmw-shell/lib/providers/Firebase/Lists'
+import Form from "../../../components/Forms/Task";
+import React, { useEffect } from "react";
+import { FormPage } from "rmw-shell/lib/containers/Page";
+import { useIntl } from "react-intl";
+import { useParams, useNavigate } from "react-router-dom";
+import { useLists } from "rmw-shell/lib/providers/Firebase/Lists";
 
-const path = 'tasks'
-const singular = 'task'
+const path = "tasks";
+const singular = "task";
 
 const Task = () => {
-  const navigate = useNavigate()
-  const intl = useIntl()
-  const { uid } = useParams()
-  const { watchList, unwatchList, getList } = useLists()
+  const navigate = useNavigate();
+  const intl = useIntl();
+  const { uid } = useParams();
+  const { watchList, unwatchList, getList } = useLists();
 
   useEffect(() => {
-    watchList('companies')
+    watchList("companies");
 
-    return () => unwatchList('companies')
-  }, [watchList, unwatchList])
+    return () => unwatchList("companies");
+  }, [watchList, unwatchList]);
 
-  const users = getList('companies')
+  const users = getList("companies");
 
-  const initialValues = { helper: '', title: '' }
+  const initialValues = { helper: "", title: "" };
 
   return (
     <FormPage
-      path={'public_tasks'}
+      path={"public_tasks"}
       uid={uid}
       initialValues={initialValues}
       getPageProps={(values) => {
         return {
           pageTitle: intl.formatMessage({
             id: path,
-            defaultMessage: 'Tasks',
+            defaultMessage: "Tasks",
           }),
-        }
+        };
       }}
       handleSubmit={(values, newUid) => {
-        console.log('values', values)
+        console.log("values", values);
         if (newUid) {
-          navigate(`/${path}/${newUid}`, { replace: true })
+          navigate(`/${path}/${newUid}`, { replace: true });
         } else {
-          navigate(`/${path}`)
+          navigate(`/${path}`);
         }
       }}
       handleDelete={() => {
-        navigate(`/${path}`)
+        navigate(`/${path}`);
       }}
       formProps={{ users }}
       Form={Form}
@@ -57,19 +57,19 @@ const Task = () => {
       deleteDialogProps={{
         title: intl.formatMessage({
           id: `delete_${singular}_dialog_title`,
-          defaultMessage: 'Delete Task?',
+          defaultMessage: "Delete Task?",
         }),
         message: intl.formatMessage({
           id: `delete_${singular}_dialog_message`,
-          defaultMessage: 'Task will be deleted permanently?',
+          defaultMessage: "Task will be deleted permanently?",
         }),
         action: intl.formatMessage({
           id: `delete_${singular}_dialog_action`,
-          defaultMessage: 'DELETE TASK',
+          defaultMessage: "DELETE TASK",
         }),
       }}
     />
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
