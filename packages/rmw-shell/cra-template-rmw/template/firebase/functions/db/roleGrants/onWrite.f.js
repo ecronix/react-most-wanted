29a6ffb6c2c7856b8e984c2d2779b30ab219c7a9
Tarik Huber <huber.tarik@gmail.com>
2023-05-24 14:57:11 +0200

    Fixed role grants cloud function

--- packages/rmw-shell/cra-template-rmw/template/firebase/functions/db/roleGrants/onWrite.f.js
+++ packages/rmw-shell/cra-template-rmw/template/firebase/functions/db/roleGrants/onWrite.f.js
@@ -1,46 +1,46 @@
-import { database } from 'firebase-functions'
-import admin from 'firebase-admin'
+import { database } from "firebase-functions";
+import admin from "firebase-admin";
 
 export default database
-  .ref('/role_grants/{roleUid}/{grantUid}')
+  .ref("/role_grants/{roleUid}/{grantUid}")
   .onWrite((eventSnapshot, context) => {
-    const roleUid = context.params.roleUid
-    const grantUid = context.params.grantUid
+    const roleUid = context.params.roleUid;
+    const grantUid = context.params.grantUid;
 
-    const userRolesRef = admin.database().ref(`user_roles`)
+    const userRolesRef = admin.database().ref(`user_roles`);
 
-    return userRolesRef.once('value').then(snapshot => {
-      let promises = []
+    return userRolesRef.once("value").then((snapshot) => {
+      let promises = [];
 
-      snapshot.forEach(userRoles => {
-        const userUid = userRoles.key
-        const roles = userRoles.val()
+      snapshot.forEach((userRoles) => {
+        const userUid = userRoles.key;
+        const roles = userRoles.val();
 
         Object.keys(roles).forEach((key, index) => {
           if (key === roleUid) {
-            let grantRef = false
+            let grantRef = false;
 
-            console.log('User role changed:', eventSnapshot.val())
+            console.log("User role changed:", eventSnapshot.after.val());
 
-            if (eventSnapshot.val()) {
+            if (eventSnapshot.after.val()) {
               grantRef = admin
                 .database()
                 .ref(`user_grants/${userUid}/${grantUid}`)
-                .set(true)
+                .set(true);
             } else {
               grantRef = admin
                 .database()
                 .ref(`user_grants/${userUid}/${grantUid}`)
-                .remove()
+                .remove();
             }
 
-            promises.push(grantRef)
+            promises.push(grantRef);
 
-            console.log('Role changed', userUid, roleUid, grantUid)
+            console.log("Role changed", userUid, roleUid, grantUid);
           }
-        })
-      })
+        });
+      });
 
-      return Promise.all(promises)
-    })
-  })
+      return Promise.all(promises);
+    });
+  });
