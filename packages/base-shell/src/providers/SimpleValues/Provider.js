696403f0597e47c2b86f647a47693425a1007da5
Tarik <tarik.huber@acid21.com>
2024-08-07 22:52:14 +0200

    Updated base shell

--- packages/base-shell/src/providers/SimpleValues/Provider.js
+++ packages/base-shell/src/providers/SimpleValues/Provider.js
@@ -1,77 +1,80 @@
-import React, { useEffect, useReducer } from 'react'
-import Context from './Context'
+import React, { useEffect, useReducer } from "react";
+import Context from "./Context";
 
 function reducer(state, action) {
-  const { type, key, value, persist } = action
+  const { type, key, value, persist } = action;
   switch (type) {
-    case 'add':
-      return { ...state, [key]: { value, persist } }
-    case 'clear':
-      const { [key]: clearedKey, ...rest } = state
-      return { ...rest }
-    case 'clear_all':
-      return {}
+    case "add":
+      return { ...state, [key]: { value, persist } };
+    case "clear":
+      const { [key]: clearedKey, ...rest } = state;
+      return { ...rest };
+    case "clear_all":
+      return {};
     default:
-      throw new Error()
+      throw new Error();
   }
 }
 
 function getInitState(persistKey) {
-  let persistedValues = {}
+  let persistedValues = {};
   try {
-    persistedValues = JSON.parse(localStorage.getItem(persistKey)) || {}
+    persistedValues =
+      JSON.parse(
+        localStorage.getItem(persistKey)?.replace("undefined", "{}") || "{}"
+      ) || {};
   } catch (error) {
-    console.warn(error)
+    console.warn(error);
   }
-  return persistedValues
+  return persistedValues;
 }
 
-const Provider = ({ children, persistKey = 'simple_values' }) => {
-  const [state, dispatch] = useReducer(reducer, getInitState(persistKey))
+const Provider = ({ children, persistKey = "simple_values" }) => {
+  const [state, dispatch] = useReducer(reducer, getInitState(persistKey));
 
   useEffect(() => {
     try {
-      const persistValues = {}
+      const persistValues = {};
 
       Object.keys(state).map((k) => {
         if (state[k].persist) {
-          persistValues[k] = { value: state[k].value, persist: true }
+          persistValues[k] = { value: state[k].value, persist: true };
         }
 
-        return k
-      })
+        return k;
+      });
 
-      localStorage.setItem(persistKey, JSON.stringify(persistValues))
+      localStorage.setItem(persistKey, JSON.stringify(persistValues));
     } catch (error) {
-      console.warn(error)
+      console.warn(error);
     }
-  }, [state, persistKey])
+  }, [state, persistKey]);
 
   const setValue = (key, value, persist = false) => {
-    dispatch({ type: 'add', key, value, persist })
-  }
+    dispatch({ type: "add", key, value, persist });
+  };
 
   const getValue = (key, defaultValue) => {
     if (state[key] !== undefined) {
-      return state[key].value
+      return state[key].value;
     } else {
-      return defaultValue
+      return defaultValue;
     }
-  }
+  };
 
   const clearValue = (key) => {
-    dispatch({ type: 'clear', key })
-  }
+    dispatch({ type: "clear", key });
+  };
 
   const clearAll = () => {
-    dispatch({ type: 'clear_all' })
-  }
+    dispatch({ type: "clear_all" });
+  };
 
   return (
     <Context.Provider value={{ setValue, getValue, clearValue, clearAll }}>
       {children}
     </Context.Provider>
-  )
-}
+  );
+};
 
-export default Provider
+export default Provider;
