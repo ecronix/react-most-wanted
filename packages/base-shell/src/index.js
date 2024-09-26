63adf932c2fad24fa28c1dfa2fb826c62a8c189e
abdallah <a@abedraba.com>
2024-09-26 23:54:49 +0200

    fix exports and publish latest version

--- packages/base-shell/src/index.js
+++ packages/base-shell/src/index.js
@@ -2,3 +2,14 @@ import App from "./containers/App/App";
 
 // App
 export default App;
+
+export { default as AuthorizedRoute } from "./components/AuthorizedRoute";
+export { default as UnauthorizedRoute } from "./components/UnauthorizedRoute";
+export * from "./providers/AddToHomeScreen";
+export * from "./providers/Auth";
+export * from "./providers/Config";
+export * from "./providers/Locale";
+export * from "./providers/Online";
+export * from "./providers/SimpleValues";
+export * from "./providers/Update";
+export * from "./utils/locale"
