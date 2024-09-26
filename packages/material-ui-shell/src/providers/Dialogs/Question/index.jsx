0677e60aa7700be4bdb0817ab6df83efd8459162
abdallah <a@abedraba.com>
2024-09-27 00:16:29 +0200

    prepare for publishing

--- packages/material-ui-shell/src/providers/Dialogs/Question/index.jsx
+++ packages/material-ui-shell/src/providers/Dialogs/Question/index.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import Context from './Context'
-export { default as withQuestions } from './with.js'
-export { default } from './Provider.js'
+export { default as withQuestions } from './with'
+export { default } from './Provider'
 
 export function useQuestions() {
   return useContext(Context)
