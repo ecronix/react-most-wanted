0c07c37fa9afb6638be729d967b507ed546c961b
abdallah <a@abedraba.com>
2024-09-28 00:29:15 +0200

    fix vite build bug

--- packages/base-shell/vite.config.js
+++ packages/base-shell/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     },
     rollupOptions: {
       input: {
-        "base-shell": resolve(__dirname, "src", "providers/index.js"),
+        "base-shell": resolve(__dirname, "src", "index.js"),
         providers: resolve(__dirname, "src", "providers/index.js"),
         utils: resolve(__dirname, "src", "utils/index.js"),
         components: resolve(__dirname, "src", "components/index.js"),
