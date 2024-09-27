dd80ef86948dd236c5404f8aad3bf39e5ca35e42
abdallah <a@abedraba.com>
2024-09-27 23:15:53 +0200

    standarize export in base-shell just like material-ui-shell

--- packages/base-shell/vite.config.js
+++ packages/base-shell/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path, { resolve } from "path";
 import { globSync } from "glob";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,30 @@ export default defineConfig({
       formats: ["es"],
     },
     rollupOptions: {
+      input: {
+        providers: resolve(__dirname, "src", "providers/index.js"),
+        utils: resolve(__dirname, "src", "utils/index.js"),
+        components: resolve(__dirname, "src", "components/index.js"),
+        ...Object.fromEntries(
+          globSync([
+            "src/components/**/*.jsx",
+            "src/containers/**/*.jsx",
+            "src/pages/**/*.jsx",
+          ])
+            .filter(
+              (file) => file.endsWith("index.jsx") || file.endsWith("index.js"),
+            )
+            .map((file) => [
+              // This remove `src/` as well as the file extension from each
+              // file, so e.g. src/nested/foo.js becomes nested/foo
+              // if it ends in index.jsx, remove that part too
+              path.relative("src", file).replace(/\/index\.jsx|\.jsx$/, ""),
+              // This expands the relative paths to absolute paths, so e.g.
+              // src/nested/foo becomes /project/src/nested/foo.js
+              fileURLToPath(new URL(file, import.meta.url)),
+            ]),
+        ),
+      },
       external: [
         "intl",
         "react",
@@ -25,22 +50,6 @@ export default defineConfig({
         "react-intl",
         "react-router-dom",
       ],
-      input: {
-        providers: resolve(__dirname, "src", "providers/index.js"),
-        utils: resolve(__dirname, "src", "utils/index.js"),
-        "containers/App": resolve(__dirname, "src", "containers/App.jsx"),
-        "containers/Layout": resolve(__dirname, "src", "containers/Layout.jsx"),
-        components: resolve(__dirname, "src", "components/index.js"),
-      },
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react-intl": "ReactIntl",
-          "react-router-dom": "ReactRouterDom",
-          intl: "Intl",
-        },
-      },
     },
   },
 });
