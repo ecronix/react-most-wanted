61e5edc49b7f2542d16a83e855436d743152c3a5
abdallah <a@abedraba.com>
2024-09-28 14:19:16 +0200

    reorganize demo content

--- packages/rmw-shell/cra-template-rmw/template/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import App from "@ecronix/base-shell/containers/App";
-import config from "./config";
-
-export default class Demo extends Component {
-  render() {
-    return <App config={config} />;
-  }
-}
--- packages/rmw-shell/cra-template-rmw/template/src/App.test.jsx
+++
@@ -1,3 +0,0 @@
-test("empty test", () => {
-  expect(1).toEqual(1);
-});
