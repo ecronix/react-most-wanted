77540ae39615cca28659c31336fe16122ce4d4ed
abdallah <a@abedraba.com>
2024-10-16 13:36:33 +0200

    standarize vite config

--- packages/material-ui-shell/vite.config.js
+++ packages/material-ui-shell/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { resolve } from 'path'
+import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer(), externalizeDeps()],
   resolve: {
     alias: {
       '@ecronix/material-ui-shell': path.resolve(__dirname, 'src'),
@@ -13,36 +15,9 @@ export default defineConfig({
   build: {
     minify: false,
     lib: {
-      entry: [],
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'material-ui-shell',
       formats: ['es'],
-    },
-    rollupOptions: {
-      input: {
-        'material-ui-shell': resolve(__dirname, 'src', 'index.js'),
-      },
-      external: [
-        'intl',
-        'react',
-        'react-dom',
-        'react-intl',
-        'react-router-dom',
-        '@ecronix/base-shell',
-        '@emotion/react',
-        '@emotion/styled',
-        '@fontsource/roboto',
-        '@mui/icons-material',
-        '@mui/material',
-        'jss-rtl',
-        'lp-react-virtualized-auto-sizer-react-18',
-        'notistack',
-        'react-custom-scrollbars-2',
-        'react-dom',
-        'react-easy-crop',
-        'react-intl',
-        'react-ios-pwa-prompt',
-        'react-markdown',
-        'react-window',
-      ],
     },
   },
 })
