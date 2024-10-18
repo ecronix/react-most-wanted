707b6d4a5882e822006d82031c40696bcedec625
abdallah <aabedraba@gmail.com>
2024-10-18 15:28:23 +0200

    add cypress test

---
+++ packages/rmw-shell/cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe("My React App", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+
+  it("renders the app", () => {
+    cy.get("#root").should("exist");
+  });
+
+  it('renders "app container top" text', () => {
+    cy.contains("React Starter-Kit with all the Most Wanted features").should(
+      "exist"
+    );
+  });
+});
